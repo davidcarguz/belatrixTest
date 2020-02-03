0edf3b85981a5a70b907713d332c9f8564daf2df
david cardenas <david.cardenas@tcs.com>
2020-02-03 13:41:18 -0500

    se obtienen todos los datos solicitados para impresión

--- src/test/java/belatrix/automation/ebay/definitions/Hooks.java
+++ src/test/java/belatrix/automation/ebay/definitions/Hooks.java
@@ -2,12 +2,24 @@ package belatrix.automation.ebay.definitions;
 
 import belatrix.automation.ebay.pageObjects.EbaySearchResultPage;
 import cucumber.api.java.After;
+import models.Product;
+
+import java.util.Comparator;
 
 public class Hooks {
 	
 	@After
 	public void afterProcedures() {
 		EbaySearchResultPage.resultsData.showResults();
+		System.out.println("//--------------First 5 Results--------------//");
+		for(int i = 0; i < 5; i++) {
+			System.out.println((i+1)+". "+EbaySearchResultPage.products.get(i).toString());
+		}
+		EbaySearchResultPage.products.sort(Comparator.comparing(Product::getPrice));
+		System.out.println("//--------------Products sort by price descendant--------------//");
+		for(int j=EbaySearchResultPage.products.size()-1; j >= 0;j--) {
+			System.out.println(EbaySearchResultPage.products.get(j).toString());
+		}
+		
 	}
-	
 }
